c711d627e5bb73f425181bd9cef2d8edca69177e
j connolly <jtsurfrat@hotmail.com>
2016-10-18 12:11:00 -0700

    add database connection to project

--- tues/createExpress/simple_crud/app.js
+++ tues/createExpress/simple_crud/app.js
@@ -5,9 +5,34 @@
 
 const express = require('express');
 
+//Include the mongodb NOdeJs driver
+
+const MongoClient = require('mongodb').MongoClient;
+
 // Create the express app
 
 const app = express ();
+
+// Create a simple shortcut way to acces the database
+
+var db;
+
+MongoClient.connect('mongodb://jtsurfrat:porter566@ds045704.mlab.com:45704/newcrud', function(error,
+database) {
+  // make sure there are no errors connecting with the DB.
+  if(error != null){
+    throw error;
+  }
+
+  // set database to the shortcut db
+
+  db = database;
+
+  console.log('Successfuly connect to the database');
+});
+
+
+//const connectionString = 'mongodb://jtsurfrat:porter566@ds045704.mlab.com:45704/newcrud';
 
 //Include the middleware / plugin that will parse the body
 // data of the the request
@@ -18,6 +43,9 @@ const bodyParser = require('body-parser');
 // of it
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+//NOte The urlend is there to the parse the raw body of the Post data and
+// attach the key value pairs to teh request object
 
 //NOTE: The urlencoded is there to the
 
@@ -42,7 +70,25 @@ app.get('/', function(request, response) {
 
 app.post('/quotes', function(request, response) {
   console.log(request.body);
+
+  //save the Post data to the mongo db.
+  db.collection('quotes').save(request.body, function(error, result) {
+
+    // check for an erro while trying to save to the database
+    if(error != null){
+      throw error;
+      console.log(error);
+      //response.send(error)
+    }
+    //redirect the user back to the home page at route '/'
+    response.redirect('/');
+  })
+
+
+
 });
+
+
 
 
 
